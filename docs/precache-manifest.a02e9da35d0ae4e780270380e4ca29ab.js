437f3c94f0028d4778a6117b7a6547c80169b476
Seba Morgado <sebaseven@gmail.com>
2020-10-03 21:59:39 -0300

    Subida con docs

---
+++ docs/precache-manifest.a02e9da35d0ae4e780270380e4ca29ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc84e65dd844a8fd5010f76334a8827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a06602e81bd87b54b69",
+    "url": "/static/css/main.aec03c19.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a06602e81bd87b54b69",
+    "url": "/static/js/main.f19ec18c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
